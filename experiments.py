39be628af24bacf61173c909dfbf58ecdd5ae2c3
Joshua Thayer <joshuathayer@riseup.net>
2018-11-18 23:26:08 -0800

    cleanup

--- experiments.py
+++ experiments.py
@@ -4,9 +4,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, \
 from PyQt5.QtCore import QObject
 import PyQt5.QtGui
 from PyQt5.QtCore import Qt
-
 import uuid
-
 from toolz.dicttoolz import dissoc, merge
 from toolz.itertoolz import get
 from functools import reduce, partial
@@ -65,30 +63,20 @@ layout1  = [{'element': 'button',
                   'id': 7777},
              ]}]
 
-
+# Recursively walk a layout, yielding each element
 def walk_elems(container):
     i = 0
 
     for ex in range(len(container['contains'])):
         e = container['contains'][ex]
 
-        prev = None
-        nxt = None
-        if ex > 0:
-            prev = container['contains'][ex-1]['id']
-        if ex < len(container['contains']) - 1:
-            nxt = container['contains'][ex+1]['id']
-
-        # yield e['id'], dissoc(e, 'contains'), \
-        #     container['id'], prev, nxt
-        yield e['id'], e, \
-            container['id'], prev, nxt
+        yield e['id'], e, container['id']
 
         if 'contains' in e:
             yield from walk_elems(e)
 
 def merge_dicts(acc, new_elem):
-    [eid, elem, elem_container, prev, nxt] = new_elem
+    [eid, elem, elem_container] = new_elem
     new_dict = {eid: {'element': elem,
                       'container': elem_container}}
     merged = merge(acc, new_dict)
@@ -97,20 +85,27 @@ def merge_dicts(acc, new_elem):
 def elem_map(container):
     return reduce(merge_dicts, walk_elems(container), {})
 
+# Determine the disposition of each element in a list of elements,
+# where that list of elements are all contained together in a single
+# container (parent). This function yields an ordered stream of
+# commands which should be used to mutate the Qt layout. It calls
+# itself recursively.
+# - l0 and l1 are ordered lists of element IDs: the contained elements
+#   in the container in the layout we're moving _from_ (l0) and moving
+#   _to_ (l1)
+# - new, rmd, and moved are sets of element IDs representing those
+#   elements that are new, removed, and moved between container in the
+#   overall layout
+# - was_reordered is the list of elements we've found which were
+#   reordered in the container- this always starts empty and is added
+#   to as those elements are found during our recursive calls.
+# - container is the element ID of the container which holds all these
+#   elements
 def find_reordered(l0, l1, new, rmd, moved,
                    l0ix, l1ix, was_reordered, container):
-    # print("----vv---")
-    # print("{} {}".format(l0, l0ix))
-    # print("{} {}".format(l1, l1ix))
-    # print("{} {}".format(l0, l0ix))
-    # print("{} {}".format(l1, l1ix))
-    # print(len(l0))
-    # print(was_reordered)
-    # print("len0 {}".format(len(l0)))
-    # print("len1 {}".format(len(l1)))
 
+    # new or moved-into?
     if len(l1) > 0 and (l1[l1ix] in new or l1[l1ix] in moved):
-        # print("SIDE EFFECT ADD/MOVE-IN {} at {} container {}".format(l1[l1ix], l1ix, container))
 
         if l1[l1ix] in new:
             yield ['add', l1[l1ix], l1ix, container]
@@ -118,7 +113,6 @@ def find_reordered(l0, l1, new, rmd, moved,
         if l1[l1ix] in moved:
             yield ['reattach', l1[l1ix], l1ix, container]
 
-        # >= catches case where l0 is empty
         if l0ix >= len(l0) - 1 and l1ix >= len(l1) - 1:
             return
 
@@ -130,11 +124,12 @@ def find_reordered(l0, l1, new, rmd, moved,
                                   min(len(l1)-1, l1ix+1),
                                   was_reordered, container)
 
+    # unchanged?
     elif len(l1) > 0 and (len(l0) > 0 and l0[l0ix] == l1[l1ix]):
-        # they're the same element
-        # print("{} and {} are the same".format(l0[l0ix], l1[l1ix]))
+
         if l0ix >= len(l0) -1 and l1ix >= len(l1) - 1:
             return
+
         yield from find_reordered(l0, l1,
                                   new,
                                   rmd,
@@ -143,12 +138,12 @@ def find_reordered(l0, l1, new, rmd, moved,
                                   min(len(l1)-1, l1ix+1),
                                   was_reordered, container)
 
+    # something that was reordered? we skip over it.
     elif len(l0) > 0 and l0[l0ix] in was_reordered:
-        # we've come across something in original list that we already
-        # detected was moved. advance over it.
-        # print("Came across already-reordered {}".format(l0[l0ix]))
+
         if l0ix >= len(l0) -1 and l1ix >= len(l1) - 1:
             return
+
         yield from find_reordered(l0, l1,
                                   new,
                                   rmd,
@@ -157,13 +152,12 @@ def find_reordered(l0, l1, new, rmd, moved,
                                   l1ix,
                                   was_reordered, container)
 
+    # removed from layout, or moved to a different container?
     elif len(l0) > 0 and (l0[l0ix] in rmd or l0[l0ix] in moved):
-        # we've come across something in the original list that was
-        # moved from this container, or removed from the
-        # layout. advance over it
-        # print("SIDE EFFECT REMOVE/MOVE-OUT {} container {}".format(l0[l0ix], container))
+
         if l0[l0ix] in rmd:
             yield ['remove', l0[l0ix], container]
+
         if l0[l0ix] in moved:
             yield ['detach', l0[l0ix], container]
 
@@ -178,10 +172,8 @@ def find_reordered(l0, l1, new, rmd, moved,
                                   l1ix,
                                   was_reordered, container)
 
+    # anything else and it's something that was reordered
     elif len(l0) > 0 and len(l1) > 0:
-        # a reordering in our container
-        # print("REORDERING SIDE EFFECT REMOVE {}".format(l1[l1ix]))
-        # print("REORDERING SIDE EFFECT ADD {} at index {} container {}".format(l1[l1ix], l1ix, container))
 
         yield ['reorder', l1[l1ix], l1ix, container]
 
@@ -198,8 +190,7 @@ def find_reordered(l0, l1, new, rmd, moved,
                                   min(len(l1)-1, l1ix+1),
                                   was_reordered, container)
 
-
-
+# This should grow as we support new elements
 def instantiate_new_elements(new_elements, element_map):
     for el in new_elements:
         if el['element']['element'] == 'label':
@@ -216,6 +207,8 @@ def instantiate_new_elements(new_elements, element_map):
 
     return element_map
 
+# Interpret the stream of commands generated by find_reordered to
+# mutate the layout
 def take_action(action, args, all_elements):
 
     if action == 'add':
@@ -236,7 +229,6 @@ def take_action(action, args, all_elements):
         else:
             c.removeWidget(e)
         e.deleteLater()
-        # del all_elements[elem]
 
     elif action == 'reorder':
         elem, pos, container = args
@@ -268,11 +260,11 @@ def take_action(action, args, all_elements):
             c.insertWidget(pos, e)
 
 
-def render_diff(l0, l1, element_map): # should return new element map
-    elemmap_0 = elem_map(l0) # native objects
-    elemmap_1 = elem_map(l1) # native objects
-    # print("elemmap 1 {}".format(elemmap_1))
-    # print("element map {}".format(element_map))
+def render_diff(l0, l1, element_map):
+
+    # generate maps of IDs to dicts representing widgets
+    elemmap_0 = elem_map(l0)  # native objects
+    elemmap_1 = elem_map(l1)  # native objects
 
     elems_0 = set(elemmap_0.keys())
     elems_1 = set(elemmap_1.keys())
@@ -306,11 +298,6 @@ def render_diff(l0, l1, element_map): # should return new element map
         if e0['container'] != e1['container']:
             moved.add(e0['element']['id'])
 
-    # print("new elements: {}".format(new_elems))
-    # print("rm elements: {}".format(rm_elems))
-    # print("changed element: {}".format(changed))
-    # print("moved element: {}".format(moved))
-
     actions = find_reordered(list(map(lambda x: x['id'], l0['contains'])),
                              list(map(lambda x: x['id'], l1['contains'])),
                              new_elems, rm_elems, moved, 0, 0, set(),
@@ -318,51 +305,37 @@ def render_diff(l0, l1, element_map): # should return new element map
 
     all_elems = merge(elemmap_0, elemmap_1)
 
-    # containers = filter(lambda x: 'contains'
-    #                     in elemmap_1[x]['element'], elems_1)
-
     containers = filter(lambda x: 'contains'
                         in all_elems[x]['element'], set(all_elems.keys()))
 
     def do_action(the_actions, em0, em1):
+
         if the_actions is None:
             return
 
         for a in the_actions:
-            print(a)
             take_action(a[0], a[1:], element_map)
 
-        print("in do action, em0 is {}".format(em0))
-        print("in do action, em1 is {}".format(em1))
         all_elems = merge(em0, em1)
 
         containers = filter(lambda x: 'contains'
-                            in all_elems[x]['element'], set(all_elems.keys()))
+                            in all_elems[x]['element'],
+                            set(all_elems.keys()))
 
         for cid in containers:
 
+            e0 = []
             if cid in em0:
                 e0 = list(map(lambda x: x['id'],
                               em0[cid]['element']['contains']))
 
-            else:
-                e0 = []
-
+            e1 = []
             if cid in em1:
                 e1 = list(map(lambda x: x['id'],
                               em1[cid]['element']['contains']))
-            else:
-                e1 = []
 
-            print("RECURSE CONTAINED 0 {}".format(e0))
-            print("RECURSE CONTAINED 1 {}".format(e1))
-
-            e1_dict = {}
-            e0_dict = {}
-            for el_id in e0:
-                e0_dict[el_id] = all_elems[el_id]
-            for el_id in e1:
-                e1_dict[el_id] = all_elems[el_id]
+            e0_dict = {el_id: all_elems[el_id] for el_id in e0}
+            e1_dict = {el_id: all_elems[el_id] for el_id in e1}
 
             new_actions = find_reordered(e0,
                                          e1,
@@ -373,6 +346,7 @@ def render_diff(l0, l1, element_map): # should return new element map
                                          0,
                                          set(),
                                          cid)
+
             do_action(new_actions, e0_dict, e1_dict)
 
     do_action(actions, elemmap_0, elemmap_1)
